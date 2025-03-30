299e9958505be609d6b449a6773cdbd15538a345
Kamil Zbikowski <kamil.zbikowski@processifier.com>
2025-03-30 16:30:45 +0200

    Update Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/tatry-home-reservation/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
